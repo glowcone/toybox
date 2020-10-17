31cfc1eb29ca35fe45e1037580142807c959a23f
whykatherine <whykatherine@gmail.com>
2020-10-17 17:34:26 +0800

    very very basic player/shooting/camera controls

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour {
+    public Rigidbody projectile;
+    public Transform Spawnpoint;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if(Input.GetButtonDown("Fire1")){
+            Rigidbody clone;
+            clone = (Rigidbody)Instantiate(projectile, Spawnpoint.position, projectile.rotation);
+
+            clone.velocity = Spawnpoint.TransformDirection(Vector3.forward*20);
+        }
+    }
+}
