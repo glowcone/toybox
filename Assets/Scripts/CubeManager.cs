01823e60da0ae27eb304d2168397c0f9fac97c8d
Steph Ng <recurza@gmail.com>
2020-10-17 14:22:03 +0800

    Initial commit

---
+++ Assets/Scripts/CubeManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeManager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,11 @@
+using System;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _walls = new GameObject[4];
+    private void Start()
+    {
+        
+    }
+}
