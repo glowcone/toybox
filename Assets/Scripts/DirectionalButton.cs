dfc868141c53dc7001510ad9036b7c185a37a6ca
Steph Ng <recurza@gmail.com>
2020-10-17 22:26:14 +0800

    Add model and update room generation code

---
+++ Assets/Scripts/DirectionalButton.cs
@@ -0,0 +1,19 @@
+using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class DirectionalButton : MonoBehaviour
+{
+    private static Vector2[] DIRECTIONS = new []{Vector2.up, Vector2.down, Vector2.right, Vector2.left};
+
+    private Vector2 _direction;
+    private void Start()
+    {
+        _direction = DIRECTIONS[Random.Range(0, DIRECTIONS.Length)];
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        CubeManager.INSTANCE.ShiftCubes(transform.position, _direction);
+    }
+}
---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,9 @@
+using System;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    private void Update()
+    {
+    }
+}
