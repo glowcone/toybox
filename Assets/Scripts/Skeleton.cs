0f943ea1c62c09978681738fa9ec20a67405148f
bethany <bethany@usc.edu>
2020-10-18 23:22:14 +0800

    skeleton spawning + health

---
+++ Assets/Scripts/Skeleton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skeleton : MonoBehaviour
+{
+    int health = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckDeath();
+    }
+
+    void CheckDeath()
+    {
+        if (health <= 0)
+            gameObject.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Bullet"))
+        {
+            health -= 1;
+            other.gameObject.SetActive(false);
+        }
+    }
+}
