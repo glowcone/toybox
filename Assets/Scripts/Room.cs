d5c45950f8543ef5e959cd899114b5e631bae265
whykatherine <whykatherine@gmail.com>
2020-10-18 21:07:15 +0800

    spawn skeletons

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -5,6 +5,12 @@ using Random = UnityEngine.Random;
 public class Room : MonoBehaviour
 {
     [SerializeField] private GameObject[] _walls = new GameObject[4];
+
+    [SerializeField] private GameObject player;
+    [SerializeField] private int MAXENEMIES = 1;
+    [SerializeField] private GameObject[] enemyPrefabs;
+    private GameObject[] enemies;
+
     private void Start()
     {
         foreach (var wall in _walls)
@@ -13,5 +19,12 @@ public class Room : MonoBehaviour
         }
         var rand = Random.Range(0, _walls.Length);
         _walls[rand].SetActive(true);
+
+        int numEnemies = (int)Random.Range(0, MAXENEMIES);
+        enemies = new GameObject[numEnemies];
+        for (int i = 0; i < numEnemies; i++) {
+            GameObject enemy = enemyPrefabs[Random.Range(0, enemyPrefabs.Length - 1)];
+            enemies[i] = Instantiate(enemy, transform.position, Quaternion.identity);
+        }
     }
 }
