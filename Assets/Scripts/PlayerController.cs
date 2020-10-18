51db43308bfd3b4770ee0f51fa0eb9a3a74a0b2c
whykatherine <whykatherine@gmail.com>
2020-10-18 14:49:59 +0800

    controls and animations for character model

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,16 +6,20 @@ public class PlayerController : MonoBehaviour
 {
     // https://docs.unity3d.com/ScriptReference/CharacterController.Move.html
 
-    private CharacterController controller;
+    [SerializeField] private CharacterController controller;
     private Vector3 playerVelocity;
     // private bool groundedPlayer;
     private float playerSpeed = 2.0f;
     // private float jumpHeight = 1.0f;
     // private float gravityValue = -9.81f;
 
+    public Animator anim;
+
     private void Start()
     {
-        controller = gameObject.AddComponent<CharacterController>();
+        // controller = gameObject.AddComponent<CharacterController>();
+        // controller.skinWidth = 0.03f;
+        // controller.height = 0.005f;
     }
 
     void Update()
@@ -28,7 +32,10 @@ public class PlayerController : MonoBehaviour
         //     playerVelocity.y = 0f;
         // }
 
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(horizontal, 0, vertical);
         controller.Move(move * Time.deltaTime * playerSpeed);
 
         if (move != Vector3.zero)
@@ -45,5 +52,8 @@ public class PlayerController : MonoBehaviour
 
         // playerVelocity.y += gravityValue * Time.deltaTime;
         // controller.Move(playerVelocity * Time.deltaTime);
+
+        anim.SetFloat("Vertical", vertical);
+        anim.SetFloat("Horizontal", horizontal);
     }
 }
