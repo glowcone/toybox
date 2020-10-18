45f5c1f8ae5e043e2ed16668f7e043c6a8099753
whykatherine <whykatherine@gmail.com>
2020-10-19 01:09:21 +0800

    skeleton health bar

---
+++ Assets/Scripts/EnemyHealthBar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealthBar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // https://forum.unity.com/threads/floating-health-bar-above-enemy.281076/
+        // Vector3 screenPos = GetComponent<Camera>().WorldToScreenPoint(target.position);
+    }
+}
--- Assets/Scripts/Skeleton.cs
+++ Assets/Scripts/Skeleton.cs
@@ -1,27 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Skeleton : MonoBehaviour
 {
-    int health = 3;
+    private const int MAXHEALTH = 3;
+    private int health;
+    [SerializeField] private Slider healthBar;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        health = MAXHEALTH;
+        healthBar.maxValue = MAXHEALTH;
     }
 
     // Update is called once per frame
     void Update()
     {
+        healthBar.value = health;
         CheckDeath();
     }
 
     void CheckDeath()
     {
         if (health <= 0)
-            gameObject.SetActive(false);
+        {
+            Destroy(healthBar.gameObject);
+            // gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
     }
 
     void OnTriggerEnter(Collider other)
